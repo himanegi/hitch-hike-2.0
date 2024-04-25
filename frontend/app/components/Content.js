617eee2f55b4f9e8749297305cad273c0deafa23
Himanshu Negi <himanegi2468@gmail.com>
2024-04-26 00:25:56 +0530

    fix routes and styles

--- frontend/app/components/Content.js
+++ frontend/app/components/Content.js
@@ -16,46 +16,49 @@ export function Content() {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const { latitude, longitude } = position.coords;
-            const map = new mapboxgl.Map({
-              container: mapContainerRef.current,
-              style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
-              center: [longitude, latitude], // Set map center to user's location
-              zoom: 14,
-            });
-
-            setMap(map);
+            if (mapContainerRef.current) {
+              const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
+                center: [longitude, latitude], // Set map center to user's location
+                zoom: 14,
+              });
+              setMap(map);
+            }
           },
           (error) => {
             console.error("Error getting user's location:", error);
             // Fallback to default location if user's location is not available
-            const map = new mapboxgl.Map({
-              container: mapContainerRef.current,
-              style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
-              center: [-70.9, 42.35], // Default location
-              zoom: 14,
-            });
-
-            setMap(map);
+            if (mapContainerRef.current) {
+              const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
+                center: [-70.9, 42.35], // Default location
+                zoom: 14,
+              });
+              setMap(map);
+            }
           }
         );
       } else {
         // Browser doesn't support Geolocation
         console.error("Geolocation is not supported by this browser.");
         // Fallback to default location
-        const map = new mapboxgl.Map({
-          container: mapContainerRef.current,
-          style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
-          center: [-70.9, 42.35], // Default location
-          zoom: 14,
-        });
-
-        setMap(map);
+        if (mapContainerRef.current) {
+          const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: "mapbox://styles/himanegi/clv9llq4j00q901ocgx2a6new",
+            center: [-70.9, 42.35], // Default location
+            zoom: 14,
+          });
+          setMap(map);
+        }
       }
     };
 
     getUserLocation();
 
-    return () => map && map.remove();
+    return () => map && map.remove(); // Clean up the map instance on component unmount
   }, []);
 
   return (
