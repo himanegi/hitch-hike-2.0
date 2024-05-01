7fb5ed669aea7d21d56a2610179dc773a4bbcebd
Raunak chauhan <raunak.2022ca077@mnnit.ac.in>
2024-05-01 15:02:13 +0530

    ride request bug resolved

--- backend/controllers/rideController.js
+++ backend/controllers/rideController.js
@@ -115,7 +115,7 @@ const getAngle = (l1, l2) => {
 
 const searchRide = async (req, res) => {
   try {
-    const { source, destination,rider } = req.body;
+    const { source, destination } = req.body;
     const line2 = {
       type: "LineString",
       coordinates: [source, destination],
@@ -123,19 +123,16 @@ const searchRide = async (req, res) => {
     const srcPt = turf.point(source);
     const destPt = turf.point(destination);
     const All_rides = await Ride.find({});
+    
+    
     const rides = await All_rides.filter((ride) => {
       const line = ride.route;
       const srcDistance = distanceFromPoint(srcPt, line);
       const destDistance = distanceFromPoint(destPt, line);
-      const alreadyRequested = ride.rideRequests.some((request) => {
-        return request.riderId == rider;
-      });
-      
-
+     
       const angle = getAngle(line, line2);
-      console.log(angle);
       if( srcDistance < 50 && destDistance < 50 && angle < 15){
-        return {...ride,alreadyRequested};
+        return true;
       } 
       return false;
     });
