f22848bc188d1f6ead4deb2d5e26fc6e47fe6088
Raunak chauhan <raunak.2022ca077@mnnit.ac.in>
2024-05-06 01:04:10 +0530

    bug fix routeLine

--- frontend/app/share/page.js
+++ frontend/app/share/page.js
@@ -32,15 +32,15 @@ const ShareComponent = () => {
     const regex = new RegExp("^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$");
   const stateCodes = ['AP', 'AR', 'AS', 'BR', 'CG', 'DL', 'GA', 'GJ', 'HR', 'HP', 'JK', 'JH', 'KA', 'KL', 'LD', 'MP', 'MH', 'MN', 'ML', 'MZ', 'NL', 'OD', 'PY', 'PB', 'RJ', 'SK', 'TN', 'TS', 'TR', 'UP', 'UK', 'WB', 'AN', 'CH', 'DN', 'DD', 'LA'];
 
-  if (!regex.test(carNumber)) {
-    alert('Invalid car number format.');
-    return;
-  }
+  // if (!regex.test(carNumber)) {
+  //   alert('Invalid car number format.');
+  //   return;
+  // }
 
-  if (!stateCodes.includes(carNumber.slice(0, 2))) {
-    alert('Invalid state code in car number.');
-    return;
-  }
+  // if (!stateCodes.includes(carNumber.slice(0, 2))) {
+  //   alert('Invalid state code in car number.');
+  //   return;
+  // }
     // Handle form submission here
     await axios
       .post("/api/rides/create", {
