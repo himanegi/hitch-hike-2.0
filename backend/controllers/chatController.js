1c1e8bb4e4b22be9cbdba7e143f0b15693828b8c
Raunak chauhan <raunak.2022ca077@mnnit.ac.in>
2024-05-12 07:29:50 +0530

    working on chat connection

---
+++ backend/controllers/chatController.js
@@ -0,0 +1,16 @@
+// chatConnection.js
+import socketIo from 'socket.io';
+
+let io;
+
+export const init = (httpServer) => {
+  io = socketIo(httpServer);
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error('Socket.io not initialized!');
+  }
+  return io;
+};
